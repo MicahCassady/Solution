aaf0b510d1327d2f1873e49fcede74d1cb35a73f
Micah Cassady <mcassady@horatioschools.org>
2020-07-29 15:35:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    //This is a solution to a problem
+    int count = 0;
+    for(int x = 1; x < 1000; x++)
+    {
+      if(x % 3 == 0 || x % 5 == 0)
+      {
+        count = count + x;
+      }
+    }
+    System.out.println("The sum of all integers under 10 is " + count);
+  }
+}
